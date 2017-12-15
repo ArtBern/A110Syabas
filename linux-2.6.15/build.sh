57fff06a9904a1c3fb789acc99cbf9e3362d1930
Ubuntu <artem@BuildMachine.iwvqkvkeehiebai0qeexyrytzd.ax.internal.cloudapp.net>
2017-12-15 09:16:45 +0000

    compiled kernel

--- linux-2.6.15/build.sh
+++ linux-2.6.15/build.sh
@@ -2,11 +2,11 @@
 . ./build.env
 datestr=`date +%Y-%m-%dT%H.%M.%S`
 logfile=build_${datestr}.log
-echo "Building, log: $logfile"
-exec > $logfile 2>&1
+#echo "Building, log: $logfile"
+#exec > $logfile 2>&1
 
-make kernel-source-2.6.15
+#make kernel-source-2.6.15
 
-cd linux-2.6.15
+#cd linux-2.6.15
 make
-make modules_install
+#make modules_install

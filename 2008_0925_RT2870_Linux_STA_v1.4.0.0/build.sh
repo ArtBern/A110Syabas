fa99c69a16f648ac54b7bb046473066efdbbd04e
Ubuntu <artem@BuildMachine.iwvqkvkeehiebai0qeexyrytzd.ax.internal.cloudapp.net>
2017-12-15 11:08:18 +0000

    build paths

---
+++ 2008_0925_RT2870_Linux_STA_v1.4.0.0/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+. ./build.env
+datestr=`date +%Y-%m-%dT%H.%M.%S`
+logfile=build_${datestr}.log
+#echo "Building, log: $logfile"
+#exec > $logfile 2>&1
+
+#LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/:/usr/lib/gcc/x86_64-linux-gnu/3.4.6/
+#eхport LIBRARY_PATH
+make
